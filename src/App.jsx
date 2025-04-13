dca09a90ea7dbb2cf374eb1e6afd76d2d4e80547
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-04-13 19:05:09 +0530

    Installed react router dom for enabling routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Form from "./components/Form";
+import NavbarComponent from "./components/Navbar";
 
 function App() {
-
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <>
+      <NavbarComponent />
+      <Routes>
+        <Route path="/" />
+        <Route path="/create-post" element={<Form />} />
+      </Routes>
+    </>
   );
 }
 
