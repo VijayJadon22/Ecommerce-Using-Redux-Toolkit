75b58d9548bbde2ebaed98ca9cb6fa6a9d3fc32a
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-04-13 19:04:45 +0530

    Implemented Form component for letting user create Post

---
+++ src/components/Form.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+
+const Form = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    age: "",
+    gender: "",
+  });
+
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div
+      style={{
+        border: "1px solid grey",
+        boxShadow: "5px 5px 2px grey",
+        width: "20rem",
+        padding: "1rem",
+      }}
+    >
+      <form onSubmit={handleSubmit}>
+        <div style={{marginBottom:"1rem"}}>
+          <label>Name:</label>
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleInputChange}
+            placeholder="Enter your name"
+          />
+        </div>
+        <div style={{marginBottom:"1rem"}}>
+          <label>Email:</label>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+            placeholder="Enter your email"
+          />
+        </div>
+        <div style={{marginBottom:"1rem"}}>
+          <label>Age:</label>
+          <input
+            type="number"
+            name="age"
+            value={formData.age}
+            onChange={handleInputChange}
+            placeholder="Enter your age"
+          />
+        </div>
+        <div style={{marginBottom:"1rem"}}>
+          <label>Gender:</label>
+          <select
+            name="gender"
+            value={formData.gender}
+            onChange={handleInputChange}
+            required
+          >
+            <option value="">Select Gender</option>
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+          </select>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
